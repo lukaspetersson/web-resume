703a028ae03a11a72158f5974cfd5cfb12cc3d2f
lukaspetersson <lukas.petersson.1999@gmail.com>
2019-01-29 10:39:20 +0100

    inleine problem and style

--- src/components/AppsSection.js
+++ src/components/AppsSection.js
@@ -3,6 +3,8 @@ import './AppsSection.css';
 import booklog_logo from "./../img/booklog_logo.png"
 import fallingkitten_logo from "./../img/fallingkitten_logo.png"
 import burnball_logo from "./../img/burnball_logo.png"
+import arrow_back from "./../img/arrow_back.png"
+import arrow_forward from "./../img/arrow_forward.png"
 import SmallBlock from "./SmallBlock.js";
 
 class AppsSection extends React.Component {
@@ -28,11 +30,35 @@ class AppsSection extends React.Component {
 
             <div className="appsBody">
                 <h1>My Mobile Apps</h1>
-                    <SmallBlock info={this.state.booklog}/>
-
-                    <SmallBlock info={this.state.burnBall}/>
-
-                    <SmallBlock info={this.state.fallingKitten}/>
+                <h3>oem+v oe+mo våemvm emvemvpomqeåvm pelvomepno qenpnved ko ckw owck wp cp kwo </h3>
+                <img src={arrow_back}/>
+                <div className="appsContainer">
+                    <div className="app">
+                        <SmallBlock info={this.state.bookLog}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.burnBall}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                    <div className="app">
+                        <SmallBlock info={this.state.fallingKitten}/>
+                    </div>
+                </div>
+                <img src={arrow_back}/>
 
             </div>
         );
