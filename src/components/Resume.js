37ea3cbb46cf2e4b659d1465481e8dfcdba28758
eptrlks <lukas.petersson.1999@gmail.com>
2019-02-20 17:42:17 +0100

    images

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,13 +8,18 @@ import './Resume.css';
 // import ExperianceResume from "./ExperianceResume.js";
 import BoxResume from "./BoxResume.js";
 import pepperPic from "./../img/pepper_transparent.png"
+import ericssonLogo from "./../img/ericsson_logo.png"
+import educationLogo from "./../img/education_logo.png"
+import bamseLogo from "./../img/bamse_logo.png"
+import erasmusLogo from "./../img/erasmus_logo.jpg"
+import booklogLogo from "./../img/booklog.png"
 
 class Resume extends React.Component {
     constructor(props){
             super(props)
             this.state = {
                 education: {
-                    image:pepperPic,
+                    image:educationLogo,
                     title: "Education",
                     examples: ["Engelska Skolan", "Cambridge IGCSE", "Kungsholmens gymnasium"],
                     examplesYear: ["-2015", "2014-2015", "2015-2018"],
@@ -29,14 +34,14 @@ class Resume extends React.Component {
                     subColor: "rgba(140, 8, 8, 0.7)"
                 },
                 ericsson: {
-                    image:pepperPic,
+                    image:ericssonLogo,
                     title: "Internship",
                     subTitle: ["Intern developer at Ericsson"],
                     color: "rgb(191, 33, 146)",
                     subColor: "rgba(112, 19, 85, 0.7)"
                 },
                 mobileApps: {
-                    image:pepperPic,
+                    image:booklogLogo,
                     title: "Mobile Development",
                     examples: ["Brännbollräknare", "BookLog", "FallingKitten"],
                     examplesYear: ["2018", "2018-2019", "2018"],
@@ -44,7 +49,7 @@ class Resume extends React.Component {
                     subColor: "rgba(65, 15, 86, 0.7)"
                 },
                 serviceJobs: {
-                    image:pepperPic,
+                    image:bamseLogo,
                     title: "Service jobs",
                     examples: ["Abbekås hamnkrog", "Drottningholms GK", "Bedinge GK", "TUI", "Hemköp", "Engelska skolan"],
                     examplesYear: ["summer 2015 + 2015", "2015 - 2017", "summer 2017", "summer 2018", "Oct-Nov 2018" , "Oct-Nov 2018"],
@@ -52,7 +57,7 @@ class Resume extends React.Component {
                     subColor: "rgba(13, 86, 52, 0.7)"
                 },
                 other: {
-                    image:pepperPic,
+                    image:erasmusLogo,
                     title: "Other",
                     examples: ["Driving licence", "Erasmus +", "Unga forskare"],
                     examplesYear: ["2017", "2017", "2018"],
@@ -64,63 +69,24 @@ class Resume extends React.Component {
     render() {
         return (
             <div >
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.education}/>
-            </div>
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.digitalEdge}/>
-            </div>
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.ericsson}/>
-            </div>
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.mobileApps}/>
-            </div>
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.serviceJobs}/>
-            </div>
-            <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
-                <BoxResume info={this.state.other}/>
-            </div>
-
-
-                {/*<div className="left_side">
-                    <div onClick={() => this.props.scrollfromParent("me")} >
-                      <div className="Me">
-                          <img className="profilePic" src={profilePic}/>
-                          <p className="name">Lukas Petersson</p>
-                          <p className="profession">App and Web Developer</p>
-                      </div>
-                    </div>
-                    <div onClick={this.moveTo} >
-                        <ContactResume />
-                    </div>
-                    <div onClick={() => this.props.scrollfromParent("me")} >
-                      <div className="Hobbies">
-                          <h2>Hobbies</h2>
-                          <img src={football_logo}/>
-                          <img src={code_logo}/>
-                          <img src={politics_logo}/>
-                      </div>
-                    </div>
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("me")}>
+                    <BoxResume info={this.state.education}/>
                 </div>
-                <div className="right_side" onMouseLeave={() => this.props.handlerFromParent("#EAE5E2")}>
-                    <div onMouseEnter={() => this.props.handlerFromParent(this.state.education.color)} onClick={() => this.props.scrollfromParent("me")} >
-                        <ExperianceResume info = {this.state.education}/>
-                    </div>
-                    <div onMouseEnter={() => this.props.handlerFromParent(this.state.web.color)} onClick={() => this.props.scrollfromParent("web")}>
-                        <ExperianceResume info = {this.state.web}/>
-                    </div>
-                    <div onMouseEnter={() => this.props.handlerFromParent(this.state.apps.color)} onClick={() => this.props.scrollfromParent("apps")}>
-                        <ExperianceResume info = {this.state.apps}/>
-                    </div>
-                    <div onMouseEnter={() => this.props.handlerFromParent(this.state.service.color)} onClick={() => this.props.scrollfromParent("service")}>
-                        <ExperianceResume info = {this.state.service}/>
-                    </div>
-                    <div onMouseEnter={() => this.props.handlerFromParent(this.state.other.color)} onClick={() => this.props.scrollfromParent("me")}>
-                        <ExperianceResume info = {this.state.other}/>
-                    </div>
-                </div>*/}
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("web")}>
+                    <BoxResume info={this.state.digitalEdge}/>
+                </div>
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("web")}>
+                    <BoxResume info={this.state.ericsson}/>
+                </div>
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("apps")}>
+                    <BoxResume info={this.state.mobileApps}/>
+                </div>
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("service")}>
+                    <BoxResume info={this.state.serviceJobs}/>
+                </div>
+                <div className="boxContainer" onClick={() => this.props.scrollfromParent("me")}>
+                    <BoxResume info={this.state.other}/>
+                </div>
             </div>
         );
     }
