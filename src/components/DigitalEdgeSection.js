f990a35774aca9e5e1d681186e7409b4e7fbd420
Lukas <Lukas.petersson.1999@gmail.com>
2019-02-26 19:13:36 +0100

    new components

---
+++ src/components/DigitalEdgeSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DigitalEdgeSection.css';
+
+class DigitalEdgeSection extends React.Component {
+    constructor(props){
+            super(props)
+            this.state = {
+            }
+    }
+    render() {
+
+        return (
+            <div className="digitalEdgeBody">
+            </div>
+        );
+        }
+}
+
+export default DigitalEdgeSection;
---
+++ src/components/EricssonSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './EricssonSection.css';
+
+class EricssonSection extends React.Component {
+    constructor(props){
+            super(props)
+            this.state = {
+            }
+    }
+    render() {
+
+        return (
+            <div className="ericssonBody">
+            </div>
+        );
+        }
+}
+
+export default EricssonSection;
