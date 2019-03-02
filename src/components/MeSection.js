f5d71ee7ccd0d710c9a24ee53d4ec28dff84c128
Lukas <Lukas.petersson.1999@gmail.com>
2019-03-02 21:27:49 +0100

    read more button

--- src/components/MeSection.js
+++ src/components/MeSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MeSection.css';
+import arrow_forward from "./../img/arrow_forward.png"
 
 class MeSection extends React.Component {
     constructor(props){
@@ -12,6 +13,10 @@ class MeSection extends React.Component {
               <div className="infoContainer">
                 <h1>About me</h1>
                 <h3>oem+v oe+mo våemvm emvemvpomqeåvm pelv omepno qenpnved ko ckw owck wp cp kwo spncåqsoåc noqwcqow+ opbncqow+pncb+ qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoq wcqow+opbncqow+pncb+qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoqwcqow+opbncqow+pncb+qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoqwcqow+opbncqow+pncb+qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoqw cqow+opb ncq ow+pncb+qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoqwcq ow+opbncqow +pncb+qowpnc pwi cie vew   n099 h 9 09 0vomepno qenpnved ko ckw owck wp cp kwo spncåqsoåcnoqwc qow+opbn cqow+pncb+qowpnc pwi cie vew   n099 h 9 09 0</h3>
+              </div>
+              <div className="readMoreContainer" onClick={()=>{window.open("https://www.w3schools.com")}}>
+                <h3>Read more</h3>
+                <img src={arrow_forward}/>
               </div>
             </div>
         );
