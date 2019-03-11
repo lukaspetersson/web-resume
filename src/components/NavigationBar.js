a5f9f8b0f8bb066ab44514bf23f9b8dfcf2b2982
eptrlks <lukas.petersson.1999@gmail.com>
2019-03-11 10:36:22 +0100

    style

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,9 +10,9 @@ class NavigationBar extends React.Component {
     }
 
     render() {
+        // <span onClick={() => this.props.scrollfromParent("top")}>Top</span>
         return (
-            <div className="bar">
-                <span onClick={() => this.props.scrollfromParent("top")}>Top</span>
+            <div className="navBar">
                 <span onClick={()=>{window.open("https://www.w3schools.com")}}>Compleate resume</span>
                 <span onClick={() => this.props.scrollfromParent("contact")}>Contact</span>
             </div>
