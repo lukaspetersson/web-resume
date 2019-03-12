41a9960d1823f083ce001bb5dd80702b48d5e41c
eptrlks <lukas.petersson.1999@gmail.com>
2019-03-12 09:53:13 +0100

    dowload page and toggle

---
+++ src/components/DowloadPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './DowloadPage.css';
+
+
+class DowloadPage extends React.Component {
+    constructor(props){
+            super(props)
+            this.state = {
+
+            }
+    }
+
+    render() {
+        return (
+            <div className="downloadPageBody">
+                sdcwqcwq
+            </div>
+        );
+    }
+}
+
+export default DowloadPage;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './NavigationBar.css';
 import menu_icon from "./../img/menu_icon.png"
+import DowloadPage from "./DowloadPage.js";
 
 
 class NavigationBar extends React.Component {
     constructor(props){
             super(props)
             this.state = {
-
+                downloadVisibility: false
             }
+            this.visibilityToggle = this.visibilityToggle.bind(this)
+    }
+
+    visibilityToggle(){
+        this.setState({
+            downloadVisibility: !this.state.downloadVisibility
+        })
     }
 
     render() {
-        // <span onClick={() => this.props.scrollfromParent("top")}>Top</span>
         return (
             <div className="navBar">
-                <img src={menu_icon}/>
+                <img src={menu_icon} onClick={this.visibilityToggle}/>
                 <span onClick={()=>{window.open("https://www.w3schools.com")}}>Compleate resume</span>
                 <span onClick={() => this.props.scrollfromParent("contact")}>Contact</span>
+                <div style={{visibility : this.state.downloadVisibility? "visible" : "hidden"}}>
+                    <DowloadPage/>
+                </div>
             </div>
         );
     }
