64ec12c14b17d75fa1a3093cb88220c4f4470ed5
eptrlks <lukas.petersson.1999@gmail.com>
2019-03-14 10:56:15 +0100

    kraftwerk gif

--- src/components/DigitalEdgeSection.js
+++ src/components/DigitalEdgeSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DigitalEdgeSection.css';
 import pepper_hello from "./../img/pepper_hello.png"
+import we_are_the_robots from "./../img/we_are_the_robots.gif"
 
 class DigitalEdgeSection extends React.Component {
     constructor(props){
@@ -12,7 +13,10 @@ class DigitalEdgeSection extends React.Component {
 
         return (
             <div className="digitalEdgeBody">
-                <img src={pepper_hello}/>
+                <div className="pepperImg">
+                    <img src={pepper_hello}/>
+                    <img src={we_are_the_robots}/>
+                </div>
                 <div>
                   <h1>Digital Edge</h1>
                   <h3>After spending much of my freetime teaching myself to code I got a job as a developer at a startup called Digital Edge, my hobby had become my full time job. At Digital Edge I have been programming a humanoid robot so that it can work as a receptionist. I worked full time for 2 months, but then switched to working weekends during my internship at Ericsson.</h3>
