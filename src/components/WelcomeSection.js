3e5e6640a642039d4d5292702d9cf12d17d047d8
eptrlks <lukas.petersson.1999@gmail.com>
2019-03-20 11:13:21 +0100

    animation

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -5,14 +5,16 @@ import down_logo from "./../img/down_logo.png"
 class WelcomeSection extends React.Component {
     constructor(props){
             super(props)
-            this.state = {}
+            this.state = {
+                margin: "0px"
+            }
     }
     render() {
         return (
             <div className="welcomeBody">
                 <div className="welcomeText">
                     <p>Welcome!</p>
-                    <img src={down_logo} onClick={() => this.props.scrollfromParent("me")}/>
+                    <img src={down_logo} style={{marginTop: this.state.margin}} onClick={() => this.props.scrollfromParent("me")} onMouseEnter={()=>{this.setState({margin:"10px"})}} onMouseLeave={()=>{this.setState({margin:"0px"})}}/>
                 </div>
             </div>
 
