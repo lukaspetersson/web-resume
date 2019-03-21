9dd3fc6e8f2ab4a72e64fdb0edcee1ffc3d7e444
eptrlks <lukas.petersson.1999@gmail.com>
2019-03-21 13:59:15 +0100

    barcelona container

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -12,6 +12,13 @@ class WelcomeSection extends React.Component {
     render() {
         return (
             <div className="welcomeBody">
+            <div className="barcaContainer">
+                <p>Barcelona friendly switch</p>
+                <label className="switch">
+                      <input type="checkbox"/>
+                      <span class="slider round"></span>
+                </label>
+            </div>
                 <div className="welcomeText">
                     <img alt="" src={down_logo} style={{marginTop: this.state.margin}} onClick={() => this.props.scrollfromParent("me")} onMouseEnter={()=>{this.setState({margin:"20px"})}} onMouseLeave={()=>{this.setState({margin:"0px"})}}/>
                 </div>
