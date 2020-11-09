86a162fb9affc20be77fa1c02ca85c86e7a318dc
Lukas <Lukas.petersson.1999@gmail.com>
2020-11-09 14:32:41 +0100

    work section

--- src/components/ProgWorkSection.js
+++ src/components/ProgWorkSection.js
@@ -4,50 +4,60 @@ import pepper_hello from "./../img/pepper_hello.png"
 import hello_animation from "./../img/hello_animation.gif"
 
 class ProgWorkSection extends React.Component {
-    constructor(props){
-            super(props)
-            this.state = {
-            }
-    }
-    isMobile(){
-        var check = false;
-          (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-          return check;
-    }
-    render() {
+	constructor(props){
+		super(props)
+		this.state = {
+		}
+	}
+	isMobile(){
+		var check = false;
+		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	}
+	render() {
 
-        var widthLimit = this.isMobile()? 550 : 510;
-        return (
-            <div className="workBody">
+		var widthLimit = this.isMobile()? 550 : 510;
+		return (
+			<div className="workBody">
 				<h1>Engineering jobs</h1>
 				<div className="space">
-	              <div>
-	                <h2>Swedish Institute of Space Physics</h2>
-	                <h3>Working at two companies so different in terms of size was a great experience! It gave me valuable insight into what motivates me. As an intern the primary goal is to learn things. I worked with WebRTC and I built web apps with React.js and an Android app with Kotlin.</h3>
-	              </div>
-	            </div>
-                <div className="pepperImg">
-                    <img alt="" src={pepper_hello}/>
-                    <img alt="" src={hello_animation}/>
-                </div>
-                <div>
-                  <h2>Lunicore</h2>
-                  <h3>Alongside my engineering studies I am working as a IT-consultant for Lunicore. The application process really put my ability to learn things to the test. I had to write a web database from scratch, something I had never done before. I first learned how to do it, and then made the website just in time for the 72h deadline. Since then my work at Lunicore have made me comfortable in backend development.</h3>
-                </div>
-				<div>
-                  <h2>Digital Edge</h2>
-                  <h3>After spending much of my free time teaching myself to code, I got a job as a developer at Digital Edge. My hobby had become my full-time job! At Digital Edge I was programming a humanoid robot so that it can work as a receptionist. I used the front-end library Vue.js and the robot’s internal API Naoqi together with various web APIs and Python. I worked full time for 5 months and during the 4 months I had my internship at Ericsson, I worked at weekends.</h3>
-                </div>
+					<div>
+						<h2>Machine Learning Intern</h2>
+						<h4>Swedish Institute of Space Physics</h4>
+						<h4>Nov 2020 - Present, Part time</h4>
+						<p>Working at two companies so different in terms of size was a great experience! It gave me valuable insight into what motivates me. As an intern the primary goal is to learn things. I worked with WebRTC and I built web apps with React.js and an Android app with Kotlin.</p>
+					</div>
+				</div>
+				<div className="pepperImg">
+					<img alt="" src={pepper_hello}/>
+					<img alt="" src={hello_animation}/>
+				</div>
+				<div className="lunicore_DigitalEdge">
+					<div>
+						<h2>Full Stack Developer</h2>
+						<h4>Lunicore</h4>
+						<h4>Nov 2019 - Present, Part time</h4>
+						<p>Alongside my engineering studies I am working as a IT-consultant for Lunicore. The application process really put my ability to learn things to the test. I had to write a web database from scratch, something I had never done before. I first learned how to do it, and then made the website just in time for the 72h deadline. Since then my work at Lunicore have made me comfortable in backend development.</p>
+					</div>
+					<div>
+						<h2>Junior Software Developer</h2>
+						<h4>Digital Edge</h4>
+						<h4>Nov 2018 - Jul 2019, Full time</h4>
+						<p>After spending much of my free time teaching myself to code, I got a job as a developer at Digital Edge. My hobby had become my full-time job! At Digital Edge I was programming a humanoid robot so that it can work as a receptionist. I used the front-end library Vue.js and the robot’s internal API Naoqi together with various web APIs and Python. I worked full time for 5 months and during the 4 months I had my internship at Ericsson, I worked at weekends.</p>
+					</div>
+				</div>
 
 				<div className="ericsson">
-	              <div>
-	                <h2>Ericsson</h2>
-	                <h3>Working at two companies so different in terms of size was a great experience! It gave me valuable insight into what motivates me. As an intern the primary goal is to learn things. I worked with WebRTC and I built web apps with React.js and an Android app with Kotlin.</h3>
-	              </div>
-	            </div>
-            </div>
-        );
-        }
+					<div>
+						<h2>Software Developer Intern</h2>
+						<h4>Ericsson</h4>
+						<h4>Jan 2019 - May 2019, Full time</h4>
+						<p>Working at two companies so different in terms of size was a great experience! It gave me valuable insight into what motivates me. As an intern the primary goal is to learn things. I worked with WebRTC and I built web apps with React.js and an Android app with Kotlin.</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ProgWorkSection;
