8418790b03e1970e87bee2918ef349b0a9413f21
Lukas <Lukas.petersson.1999@gmail.com>
2020-11-10 17:00:44 +0100

    remove unused images and scroll btns

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ import Skills from './experiance/Skills.js'
 import Involvement from './experiance/Involvement.js'
 import Education from './experiance/Education.js'
 import Projects from './experiance/Projects.js'
-import up_icon from "./../img/up_icon.svg"
 
 class FrontPage extends React.Component {
     constructor(props){
@@ -22,15 +21,14 @@ class FrontPage extends React.Component {
                 menuColor:"#ffffff",
                 coverHeight: 500,
             }
-            this.refResume = React.createRef()
-            this.refResume = React.createRef()
+            this.refContent = React.createRef()
             this.refEducation = React.createRef()
-            this.refDigitalEdge = React.createRef()
-            this.refEricsson = React.createRef()
-            this.refApps = React.createRef()
-            this.refService = React.createRef()
-            this.refOther = React.createRef()
+            this.refEngineeringJobs = React.createRef()
+            this.refProjects = React.createRef()
+            this.refInvolvement = React.createRef()
+            this.refSkills = React.createRef()
             this.refContact = React.createRef()
+			this.refOtherJobs = React.createRef()
 
             this.handleScroll = this.handleScroll.bind(this);
             this.setCoverHeight = this.setCoverHeight.bind(this);
@@ -58,26 +56,26 @@ class FrontPage extends React.Component {
             case "top":
                 window.scrollTo(0, 0);
                 break;
-            case "resume":
-                window.scrollTo(0, this.refResume.current.offsetTop);
+            case "content":
+                window.scrollTo(0, this.refContent.current.offsetTop);
                 break;
             case "education":
                 window.scrollTo(0, this.refEducation.current.offsetTop);
                 break;
             case "engJobs":
-                window.scrollTo(0, this.refDigitalEdge.current.offsetTop);
+                window.scrollTo(0, this.refEngineeringJobs.current.offsetTop);
                 break;
-            case "ericsson":
-                window.scrollTo(0, this.refEricsson.current.offsetTop);
+            case "projects":
+                window.scrollTo(0, this.refProjects.current.offsetTop);
                 break;
-            case "apps":
-                window.scrollTo(0, this.refApps.current.offsetTop);
+            case "involvement":
+                window.scrollTo(0, this.refInvolvement.current.offsetTop);
                 break;
-            case "service":
-                window.scrollTo(0, this.refService.current.offsetTop);
+            case "skills":
+                window.scrollTo(0, this.refSkills.current.offsetTop);
                 break;
-            case "other":
-                window.scrollTo(0, this.refOther.current.offsetTop);
+			case "otherJobs":
+                window.scrollTo(0, this.refOtherJobs.current.offsetTop);
                 break;
             case "contact":
                 window.scrollTo(0, this.refContact.current.offsetTop);
@@ -87,83 +85,37 @@ class FrontPage extends React.Component {
     }
     render() {
 
-        var body = document.body;
-        var html = document.documentElement;
-        var height = Math.max( body.scrollHeight, body.offsetHeight,
-                           html.clientHeight, html.scrollHeight, html.offsetHeight );
-        var atBottom = ((window.innerHeight + window.pageYOffset) >= (height - 30));
-        try{
-          if(window.pageYOffset > this.refEducation.current.offsetTop - window.innerHeight/2){
-            var sectionUp = "";
-            var sectionDown = "";
-            if(window.pageYOffset < this.refDigitalEdge.current.offsetTop - this.refEducation.current.clientHeight/2){
-                sectionUp = "resume";
-                sectionDown = "digitalEdge";
-            }else if(window.pageYOffset < this.refEricsson.current.offsetTop - this.refDigitalEdge.current.clientHeight/2){
-                sectionUp = "education";
-                sectionDown = "ericsson";
-            }else if(window.pageYOffset < this.refApps.current.offsetTop - this.refEricsson.current.clientHeight/2){
-              sectionUp = "digitalEdge";
-              sectionDown = "apps";
-            }else if(window.pageYOffset < this.refService.current.offsetTop - this.refApps.current.clientHeight/2){
-              sectionUp = "ericsson";
-              sectionDown = "service";
-            }else if(window.pageYOffset < this.refOther.current.offsetTop - this.refService.current.clientHeight/2){
-              sectionUp = "apps";
-              sectionDown = "other";
-          }else if(!atBottom){
-              sectionUp = "service";
-              sectionDown = "contact";
-            }else{
-                //contact and service is in same page, no need to scroll between
-                if(this.refOther.current.clientHeight+this.refContact.current.clientHeight < window.innerHeight){
-                    sectionUp = "service";
-                }else{
-                    sectionUp = "other";
-                }
-            }
-            if(sectionDown){
-              var downNav = <img alt="" src={up_icon} id="downNav" style={{transform: "rotate(180deg)"}} onClick={()=> {this.handleScroll(sectionDown)}}/>
-
-            }
-            var upNav = <img alt="" src={up_icon} id="upNav" onClick={()=> {this.handleScroll(sectionUp)}}/>
-          }
-        }catch{}
         return (
                 <div className="body">
-                        <div className="navigationMenu">
+                        <div className="downloadMenu">
                             <DownloadMenu scrollfromParent={this.handleScroll} menuColor={this.state.menuColor}/>
                         </div>
-                        <div className="WelcomeSection">
+                        <div className="Intro">
                             <Intro scrollfromParent={this.handleScroll} setCoverHeight={this.setCoverHeight}/>
                         </div>
-                        <div className="experiancesSections" ref={this.refResume}>
+                        <div className="experiancesSections" ref={this.refContent}>
                             <Content scrollfromParent={this.handleScroll} />
                         </div>
-						<div className="experiancesSections" ref={this.refOther}>
+						<div className="experiancesSections" ref={this.refSkills}>
                             <Skills/>
                         </div>
 						<div className="experiancesSections" ref={this.refEducation}>
                             <Education/>
                         </div>
-                        <div className="experiancesSections" ref={this.refDigitalEdge}>
+                        <div className="experiancesSections" ref={this.refEngineeringJobs}>
                             <EngineeringJobs/>
                         </div>
-						<div className="experiancesSections" ref={this.refApps}>
+						<div className="experiancesSections" ref={this.refProjects}>
                             <Projects/>
                         </div>
-						<div className="experiancesSections" ref={this.refService}>
+						<div className="experiancesSections" ref={this.refInvolvement}>
                             <Involvement/>
                         </div>
-                        <div className="experiancesSections" ref={this.refService}>
+                        <div className="experiancesSections" ref={this.refOtherJobs}>
                             <OtherJobs/>
                         </div>
                         <div className="experiancesSections" ref={this.refContact}>
                             <Contact/>
-                        </div>
-                        <div className="navigationButtons">
-                            {downNav}
-                            {upNav}
                         </div>
 
                 </div>
